24fa3604fd8b6df5c52297e1ca3f2b700b066ed3
fukmaru <k@fukmaru.com>
2019-05-04 00:11:18 +0900

    検索フォームを作る

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,13 @@
 class PagesController < ApplicationController
   def home
+    @contents = Content.where(is_published: true)
+
+    search_params = params.dig("s")
+    if search_params
+      search_params_arr = search_params.split
+      @contents = @contents.where("body ~~* any(array[#{search_params_arr.map{ |i|  %Q('%#{i}%') }.join(',')}])") if search_params_arr.present?
+    end
+
+    @search_str = search_params
   end
 end
--- frontend/packs/application.js
+++ frontend/packs/application.js
@@ -1,6 +1,8 @@
 import "init";
 import "components/page/page"
 import "components/contents/contents"
+import "components/content-search-form/content-search-form"
+import "components/content-search-result/content-search-result"
 
 import Rails from 'rails-ujs';
 Rails.start();
